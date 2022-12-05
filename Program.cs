5a639ac839c1863448486d4f65a6e14925b79537
Dmitry2707 <119356448+Dmitry2707@users.noreply.github.com>
2022-12-06 02:17:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("write number N");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 1)
+{
+    Console.WriteLine(number % 2); 
+}
+else
+{
+    Console.WriteLine("нечетное");
+}
